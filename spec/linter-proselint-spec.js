526109bc25d0e99f45f48b52d7c61abdef0bc7ea
Landon Abney <Landonabney@gmail.com>
2017-08-28 11:46:17 -0700

    Asyncify the specs
    Bring in `jasmine-fix` to allow the usage of `async`/`await` in the
    specs.

--- spec/linter-proselint-spec.js
+++ spec/linter-proselint-spec.js
@@ -1,6 +1,8 @@
 'use babel';
 
 import * as path from 'path';
+// eslint-disable-next-line no-unused-vars
+import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
 import linterProselint from '../lib';
 
 const linter = linterProselint.provideLinter();
@@ -10,30 +12,26 @@ const badPath = path.join(__dirname, 'fixtures', 'bad.txt');
 const goodPath = path.join(__dirname, 'fixtures', 'good.txt');
 
 describe('The proselint provider for Linter', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     atom.workspace.destroyActivePaneItem();
-    waitsForPromise(() => atom.packages.activatePackage('linter-proselint'));
+    await atom.packages.activatePackage('linter-proselint');
   });
 
-  it('finds nothing wrong with a valid file', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(goodPath).then(editor => lint(editor)).then((messages) => {
-        expect(messages.length).toBe(0);
-      }),
-    );
+  it('finds nothing wrong with a valid file', async () => {
+    const editor = await atom.workspace.open(goodPath);
+    const messages = await lint(editor);
+    expect(messages.length).toBe(0);
   });
 
-  it('shows issues with a problematic file', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(badPath).then(editor => lint(editor)).then((messages) => {
-        expect(messages.length).toBe(1);
+  it('shows issues with a problematic file', async () => {
+    const editor = await atom.workspace.open(badPath);
+    const messages = await lint(editor);
 
-        expect(messages[0].type).toBe('Warning');
-        expect(messages[0].text).toBe("'lol.' is chatspeak. Write it out.");
-        expect(messages[0].html).not.toBeDefined();
-        expect(messages[0].filePath).toBe(badPath);
-        expect(messages[0].range).toEqual([[0, 15], [0, 19]]);
-      }),
-    );
+    expect(messages.length).toBe(1);
+    expect(messages[0].type).toBe('Warning');
+    expect(messages[0].text).toBe("'lol.' is chatspeak. Write it out.");
+    expect(messages[0].html).not.toBeDefined();
+    expect(messages[0].filePath).toBe(badPath);
+    expect(messages[0].range).toEqual([[0, 15], [0, 19]]);
   });
 });
